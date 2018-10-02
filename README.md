2137bb7a465cb50aa5a67aad633ed08ca17af799
jspacek <jodi.spacek@gmail.com>
2018-10-02 10:05:05 -0700

    Separate out logic from run params. Add a normal state simulation.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-> My salad days,
-
-> When I was green in judgment, cold in blood,
-
-> To say as I said then! - Antony and Cleopatra
-
 ### TODO items
 
 - further the analysis in all bounds and two choice
 - add state in the agetwochoice file for normal, attack, recovery states
 - figure out some rates of attack, and how to delay the attack in a tail fashion
+- rate of attack should be conditioned on the ratio of attackers
+- refuse proxy assignments when maximum load reached, default m/n
+
+> My salad days, When I was green in judgment, cold in blood, To say as I said then! - Antony and Cleopatra
---
+++ normalrun.py
@@ -0,0 +1,57 @@
+import random
+import simpy
+import collections
+import agetwochoice
+
+SEED = 42                           # Answer to life, the universe, and everything
+NUM_PROXIES = 100                   # Default number of total proxies
+NUM_CLIENTS = 1000                  # Total number of clients
+CLIENT_ARRIVAL_MEAN = 10.0          # Average time (ms) in between client arrivals
+CLIENT_ARRIVAL_SIGMA = 2.0          # Sigma for client arrival time
+BLOCK_ARRIVAL_MEAN = 10.0           # Average time (ms) in between blocks
+BLOCK_ARRIVAL_SIGMA = 2.0           # Sigma for blocking
+SIM_TIME = 10000                    # 100 clients assigned one per second
+
+def client_arrival_rate():
+    """Clock time in between client arrivals."""
+    return random.normalvariate(CLIENT_ARRIVAL_MEAN, CLIENT_ARRIVAL_SIGMA)
+
+def single_block_rate():
+    """Clock time between blocking events of single proxies."""
+    return random.normalvariate(BLOCK_ARRIVAL_MEAN, BLOCK_ARRIVAL_SIGMA)
+
+print('Starting the age based simulation: normal state')
+random.seed(SEED)
+env = simpy.Environment()
+
+# Collections for proxy assignments with clients, vulnerable and blocked proxies
+Assignments = collections.namedtuple('Assignments', ['stepwise', 'proxies', 'vulnerable', 'blocked'])
+proxies = set(agetwochoice.Proxy(env, 'Proxy %d' % i, i, set()) for i in range(NUM_PROXIES))
+vulnerable = set()
+blocked = set()
+
+# Clients are assigned and proxies blocked as stepwise events
+stepwise = simpy.Resource(env, capacity=1)
+proxy_system = Assignments(stepwise, proxies, vulnerable, blocked)
+
+# Start assignment and blocking processes and run
+env.process(agetwochoice.client_assignment(env, proxy_system, client_arrival_rate()))
+env.process(agetwochoice.proxy_block(env, proxy_system, single_block_rate()))
+
+env.run(until=SIM_TIME)
+
+# Analysis
+for proxy in proxies:
+    print(proxy.age)
+    for client in proxy.clients:
+        print(client.malicious)
+
+for proxy in vulnerable:
+    print(proxy.age)
+    for client in proxy.clients:
+        print(client.malicious)
+
+for proxy in blocked:
+    print(proxy.age)
+    for client in proxy.clients:
+        print(client.malicious)
